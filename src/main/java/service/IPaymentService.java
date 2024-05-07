077132dfc372f579b45f32940e1112dc156ba566
yash.jain <yashjain19970@gmail.com>
2024-05-07 08:52:11 +0530

    pushing code for creating order in razorpay

--- src/main/java/service/IPaymentService.java
+++ src/main/java/service/IPaymentService.java
@@ -1,6 +1,8 @@
 package service;
 
+import com.razorpay.RazorpayException;
+
 public interface IPaymentService {
 
-    String doPayment(String email,Integer amount, String phoneNo, String orderId);
+    String doPayment(String email,Integer amount, String phoneNo, String orderId) throws RazorpayException;
 }
--- src/main/java/service/RazorpayPaymentService.java
+++ src/main/java/service/RazorpayPaymentService.java
@@ -1,40 +1,51 @@
 package service;
 
+import com.razorpay.PaymentLink;
+import com.razorpay.RazorpayClient;
+import com.razorpay.RazorpayException;
 import org.json.JSONObject;
 import org.springframework.stereotype.Service;
 
 @Service("razorpayPaymentService")
-public class RazorpayPaymentService implements IPaymentService{
+public class RazorpayPaymentService implements IPaymentService {
+    private RazorpayClient razorpayClient;
+
+    public RazorpayPaymentService(RazorpayClient razorpayClient) {
+        this.razorpayClient = razorpayClient;
+    }
+
     @Override
-    public String doPayment(String email, Integer amount, String phoneNo, String orderId) {
-        JSONObject requestBody =  getRequestBodyForRazorPay(amount, orderId);
+    public String doPayment(String email, Integer amount, String phoneNo, String orderId) throws RazorpayException {
+        // S1. constructing data
+        JSONObject requestBody = getRequestBodyForRazorPay(amount, orderId);
+        // S2. call razorpay
+        PaymentLink razorpayPaymentLink = razorpayClient.paymentLink.create(requestBody);
 
+        // may be you can store this information in your database.
+        // just to keep a track.
 
-
-        return null;
+        return  razorpayPaymentLink.toString();
     }
 
     /**
      * {
-     *     "amount" :
-     *     "currency"
-     *     "receipt"
-     *     "customer" :{
-     *         "phone" : "",
-     *         "email : "",
-     *     },
-     *     "notify" : {
-     *         "sms" : true,
-     *         "email" : true,
-     *     }
+     * "amount" :
+     * "currency"
+     * "receipt"
+     * "customer" :{
+     * "phone" : "",
+     * "email : "",
+     * },
+     * "notify" : {
+     * "sms" : true,
+     * "email" : true,
      * }
-     *
-     *
+     * }
      */
     private JSONObject getRequestBodyForRazorPay(Integer amount, String orderId) {
         JSONObject customerInfo = new JSONObject();
-        customerInfo.put("phone","9999999999");
-        customerInfo.put("email","yash.jain@gmail.com");
+        customerInfo.put("phone", "9999999999");
+        customerInfo.put("email", "yash.jain@gmail.com");
 
         // set some notification configurations
         JSONObject notify = new JSONObject();
@@ -48,6 +59,8 @@ public class RazorpayPaymentService implements IPaymentService{
         orderRequest.put("receipt", orderId); // optional
         orderRequest.put("customer", customerInfo);
         orderRequest.put("notify", notify);
+        orderRequest.put("callback_url", "https://scaler.com/razorpay/webhook");
+        orderRequest.put("callback_method", "get");
         return customerInfo;
     }
 }
